5a475cea5c8bcdac76850119445cc0dbc9433514
Jonathan Guevarez <jonag96@vt.edu>
2018-10-21 14:30:26 -0400

    added the immediete stuff and working on the first loop check

--- CO_proj_1/CO_proj_1/Registers.cpp
+++ CO_proj_1/CO_proj_1/Registers.cpp
@@ -30,6 +30,42 @@ std::string Registers::product(int opcode, int rs, int rt, int rd, int shamt, in
 	end_str = end_str + reggies[rt] + " ";
 	return end_str;
 }
+
+std::string Registers::imm_product(int opcode, int rs, int rt, int imm)
+{
+	std::string reggies[] = { "$zero", "$at", "$v0","$v1","$a0","$a1","$a2","$a3","$t0","$t1","$t2",
+						 "$t3","$t4","$t5","$t6","$t7","$s0","$s1","$s2","$s3","$s4","$s5","$s6",
+						 "$s7","$t8","$t9","$k0","$k1","$gp","$sp","$fp","$ra" };
+
+	std::string end_str = "";
+
+		 if (opcode == 8) end_str = "addi ";
+	else if (opcode == 9) end_str = "addiu ";
+	else if (opcode == 12) end_str = "andi ";
+	else if (opcode == 15) end_str = "lui ";
+	else if (opcode == 35) end_str = "lw ";
+	else if (opcode == 13) end_str = "ori ";
+	else if (opcode == 10) end_str = "slti ";
+	else if (opcode == 11) end_str = "sltiu ";
+	else if (opcode == 43) end_str = "sw ";
+	else if (opcode == 4)
+	 {
+			 end_str = "beq ";
+
+	 }
+	else if (opcode ==5) end_str = "bne ";
+		 //maybe add the wierd ones?
+
+
+
+	return end_str;
+}
+
+void Registers::address_check(int opcode, int rs, int rt, int imm)
+{
+
+}
+
 Registers::~Registers()
 {
 }
--- CO_proj_1/CO_proj_1/Registers.h
+++ CO_proj_1/CO_proj_1/Registers.h
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Registers
 {
 public:
 	Registers();
 	std::string product(int opcode, int rs, int rt, int rd, int shamt, int fucnt);
+	std::string imm_product(int opcode, int rs, int rt, int imm);
+	void address_check(int opcode, int rs, int rt, int imm);
 	
+	std::vector<int> addresses;
 	~Registers();
 };
 
